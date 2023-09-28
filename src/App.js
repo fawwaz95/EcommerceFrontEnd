d13b191d3572770a0c7ddee9897f559afa4b74e8
fawwaz95 <fawwaz_95@hotmail.com>
2023-09-27 22:35:37 -0400

    import switch

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import Shop from './components/Shop.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useLocation, Switch } from 'react-router-dom';
 
 export default function App() {
 
