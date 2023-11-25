685c3ed696544eccead02068db6c868fe46ff4c7
fawwaz95 <fawwaz_95@hotmail.com>
2023-11-25 16:22:14 -0500

    some images were smaller then others in shop, changed all images to be same height and width

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -5,7 +5,7 @@ const ShopItem = ({items}) => {
     return (
         <Link to={`/Product/${items._id}`} >
             <div id={items._id} className="border border-2 border-white">
-                <img id="prodImg" src={items.src}/>
+                <img id="prodImg" className="prodImg" src={items.src}/>
                 <div className="flex justify-between pt-5 vintageFont text-xs">
                     <p className="align-left"> {items.item}</p>
                     <div className="align-right">{items.price}</div>
